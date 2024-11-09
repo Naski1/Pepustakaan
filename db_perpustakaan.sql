b73faa448ff3a5f53af6856e2d3a26eea956c71e
Naski1 <gantengiksan65@gmail.com>
2024-11-09 21:13:59 +0700

    database perpustakaan

--- db_perpustakaan.sql
+++ db_perpustakaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2024 at 03:01 PM
+-- Generation Time: Nov 09, 2024 at 03:13 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -35,6 +35,83 @@ CREATE TABLE `jurusan` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `sequelizemeta`
+--
+
+CREATE TABLE `sequelizemeta` (
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `sequelizemeta`
+--
+
+INSERT INTO `sequelizemeta` (`name`) VALUES
+('20241019132354-create-user.js'),
+('20241020130601-create-tokens-table.js');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`id`, `username`, `password`, `role`, `createdAt`, `updatedAt`) VALUES
+(31, 'pepen', '$2a$10$pVNzDcZhFR1jtOEygsbYqO6RJiCTLliWQcvQMQ1r22HtyymBRY/ku', 'admin', '2024-10-26 14:47:26', '2024-10-26 14:47:26'),
+(32, 'iksan', '$2a$10$Dz367st/0JjHQBOjMuVMAeANs.5QN4ttu6uMroNXpR3L4HeKmuU5u', 'admin', '2024-11-06 04:46:51', '2024-11-06 04:46:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_tokens`
+--
+
+CREATE TABLE `tb_tokens` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `token` text NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `isActive` tinyint(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_tokens`
+--
+
+INSERT INTO `tb_tokens` (`id`, `userId`, `token`, `createdAt`, `updatedAt`, `isActive`) VALUES
+(6, 31, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzEsInVzZXJuYW1lIjoicGVwZW4iLCJpYXQiOjE3MzAwOTQ4MTYsImV4cCI6MTczMDA5ODQxNn0.F7nOHLTDGQczGieKGnRIaCdjHkY8tNmnu5E2jXeGcwg', '2024-10-28 05:53:36', '2024-10-28 05:53:38', 0),
+(10, 31, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzEsInVzZXJuYW1lIjoicGVwZW4iLCJpYXQiOjE3MzAxNjUwNjgsImV4cCI6MTczMDE2ODY2OH0.M90ilIWJEuPw_HIvulcSkHPO53eJ1CVdJ6j40_iYlkY', '2024-10-29 01:24:28', '2024-10-29 01:24:32', 0),
+(15, 32, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzIsInVzZXJuYW1lIjoiaWtzYW4iLCJpYXQiOjE3MzEwNzcyNDgsImV4cCI6MTczMTA4MDg0OH0.HOFQ7ZyfQDrKg2ju77JMW4pBc8e3NapCxDWiLfJIcKI', '2024-11-08 14:47:28', '2024-11-08 14:47:35', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `user_id` int(11) NOT NULL,
+  `nama` int(11) NOT NULL,
+  `jurusan_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `wilayah`
 --
 
@@ -54,6 +131,33 @@ ALTER TABLE `jurusan`
   ADD PRIMARY KEY (`jurusan_id`);
 
 --
+-- Indexes for table `sequelizemeta`
+--
+ALTER TABLE `sequelizemeta`
+  ADD PRIMARY KEY (`name`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_tokens`
+--
+ALTER TABLE `tb_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`user_id`,`jurusan_id`),
+  ADD KEY `jurusan_id` (`jurusan_id`);
+
+--
 -- Indexes for table `wilayah`
 --
 ALTER TABLE `wilayah`
@@ -70,10 +174,44 @@ ALTER TABLE `jurusan`
   MODIFY `jurusan_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `tb_tokens`
+--
+ALTER TABLE `tb_tokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `wilayah`
 --
 ALTER TABLE `wilayah`
   MODIFY `wilayah_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_tokens`
+--
+ALTER TABLE `tb_tokens`
+  ADD CONSTRAINT `tb_tokens_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `tb_admin` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD CONSTRAINT `tb_user_ibfk_1` FOREIGN KEY (`jurusan_id`) REFERENCES `jurusan` (`jurusan_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
